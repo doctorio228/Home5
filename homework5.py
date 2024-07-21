712a287ebca6395b43c287efe3d34077c9a7b7f7
doctorio228 <kukhtin.a@list.ru>
2024-07-21 17:57:41 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 'year', 0.5, True
+print(immutable_var)
+#immutable_var[0] = 50
+# изначально это неизменяемо
+mutable_list = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+mutable_list[5] = 60
+mutable_list[7] = 100
+print(mutable_list)
